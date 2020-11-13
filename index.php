b8ab36f0bd5173db8a914ac91dcb3da392590e18
vanjo-khim <khimlaude@gmail.com>
2020-11-14 02:41:19 +0800

    testing commit

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
+  class User
+  {
+      //Properties
+      public $name;
+      public $username;
+      public $email;
 
-echo "Hello world";
+     function addFriend($name){
+       echo $name . " added a friend <br/>";
+     }
 
+     function postStatus($name){
+       echo $name . " posted a status <br/>";
+     }
+  }
+  $user1 = new User();
+  $user1->addFriend('Van Joakhim B. Laude');
+  $user1-> postStatus('Van Joakhim B. Laude');
 ?>
